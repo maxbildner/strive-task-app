169babba8eacb4bdee27c44d083a6fb1ce682f20
mbildner <mbildner@icloud.com>
2020-12-30 18:29:05 -0500

    update row css, circle size

--- src/components/Icon.js
+++ src/components/Icon.js
@@ -40,12 +40,6 @@ class Icon extends React.Component {
         onMouseLeave={() => this.setState({ isMouseOverIcon: false })}
         onClick={onClickIcon}
       >
-        {/* <span
-          className={(isRowClicked || isMouseOverIcon) ? "circle" : "circle hidden"}
-        >
-        </span>
-        {svgLogo} */}
-
         <span
           className={(isRowClicked || isMouseOverIcon) ? (type === "details" ? "circle hidden" : "circle") : "circle hidden"}
         >
