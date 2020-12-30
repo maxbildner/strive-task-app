a4ca95e9ae566b7051e82b47fabf5c2b4a1961e0
mbildner <mbildner@icloud.com>
2020-12-29 21:48:34 -0500

    style delete complete detail buttons on hover

--- src/components/Task.js
+++ src/components/Task.js
@@ -65,8 +65,7 @@ class Task extends React.Component {
             <svg height="15pt" viewBox="0 -46 417.81333 417" width="15pt" xmlns="http://www.w3.org/2000/svg"><path d="m159.988281 318.582031c-3.988281 4.011719-9.429687 6.25-15.082031 6.25s-11.09375-2.238281-15.082031-6.25l-120.449219-120.46875c-12.5-12.5-12.5-32.769531 0-45.246093l15.082031-15.085938c12.503907-12.5 32.75-12.5 45.25 0l75.199219 75.203125 203.199219-203.203125c12.503906-12.5 32.769531-12.5 45.25 0l15.082031 15.085938c12.5 12.5 12.5 32.765624 0 45.246093zm0 0" /></svg>
           </span>
           <span 
-            // className={this.state.isMouseOver ? "details" : "details hidden"}
-            className="details"
+            className={this.state.isMouseOver ? "details hovered" : "details"}
           >
             <svg height="15pt" widht="15pt" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M174.54,508.94,420.1,263.39a10.74,10.74,0,0,0,0-14.75L174.53,3.06a10.43,10.43,0,0,0-14.75,0L92,70.8a10.43,10.43,0,0,0,0,14.75L262.47,256,92,426.46a10.73,10.73,0,0,0,0,14.75l67.75,67.73A10.43,10.43,0,0,0,174.54,508.94Z" /></svg>
           </span>
